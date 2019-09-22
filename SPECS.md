840e389b3f6f46b352a7c263dc861829a2101366
Jonathan Smith <jonathan@peoplesized.com>
2019-09-22 07:22:43 -0700

    add code and specs

--- SPECS.md
+++ SPECS.md
@@ -11,7 +11,8 @@ JSON object containing:
 - `raster`: _{string: Base64-encoded image file or URL}_ Source raster containing values to extract.
 - `subject`: _{GeoJSON FeatureCollection of Polygons or Points, URL}_ GeoJSON of polygons or points.
 - `stats`: _{string}_ Defaults to `mean`. For _polygons_, pass one or more of `mean`, `max`, `min`, `mode`, `sum`, `count` separated by spaces e.g. `max mean`. Not required (and ignored) for _points_.
-- `geojson_out`: _{boolean}_ Default `true`. 
+- `buffer_km`: _{optional, integer}_ If given and the `subject` contains _Points_ (or _MultiPoints_), then the stats will be calculated for a polygon buffering the point by the given distance.
+- `geojson_out`: _{boolean}_ Default `true`.
   - For _polygons_: If `true`, return the incoming GeoJSON with additional properties named as given in `stats`. If `false`, return an array of extracted values named as given in `stats`.
   - For _points_: If `true`, return the incoming GeoJSON with an additional property of `value`. If `false`, return an array of the extracted values.
 
--- function/handler.py
+++ function/handler.py
@@ -9,8 +9,9 @@ from geojson import FeatureCollection
 def run_function(params):
     # Prepare the parameters
     subject_ref = params['subject']
-    raster      = params['raster']
-    stats       = params.get('stats', 'mean')
+    raster = params['raster']
+    stats = params.get('stats', 'mean')
+    buffer_km = params.get('buffer_km')
     geojson_out = params.get('geojson_out', True)
 
     try:
@@ -18,11 +19,19 @@ def run_function(params):
         subject_types = get_subject_types(subject)
 
         if 'Polygon' in subject_types or 'MultiPolygon' in subject_types:
-            features = zonal_stats(subject, raster, stats=stats, geojson_out=geojson_out)
+            features = zonal_stats(
+                subject, raster, stats=stats, geojson_out=geojson_out)
         elif 'Point' in subject_types or 'MultiPoint' in subject_types:
-            features = point_query(subject, raster, geojson_out=geojson_out)
+            if buffer_km is not None:
+                buffer_degrees = buffer_km / 111
+                polygons = subject.buffer(buffer_degrees)
+                features = zonal_stats(
+                    polygons, raster, stats=stats, geojson_out=geojson_out)
+            else
+                features = point_query(subject, raster, geojson_out=geojson_out)
         else:
-            raise ValueError("Input features need to be either all Polygons or Points. Doesn't look like they are.")
+            raise ValueError(
+                "Input features need to be either all Polygons or Points. Doesn't look like they are.")
 
         # Decorate return as either GeoJSON FeatureCollection (other way to do this?) or just the array of stats
         if geojson_out:
@@ -31,10 +40,11 @@ def run_function(params):
             return features
     except ValueError as e:
         if e == 'Specify either bounds or window':
-            raise ValueError("One or more features are lacking geometry or have null/empty values")
+            raise ValueError(
+                "One or more features are lacking geometry or have null/empty values")
 
     except AttributeError:
-        raise ValueError("Error calculating zonalstats. Please confirm every input "
+        raise ValueError("Error calculating zonal_stats or point_query. Please confirm every input "
                          "geometry is valid, and contains coordinates")
 
 
