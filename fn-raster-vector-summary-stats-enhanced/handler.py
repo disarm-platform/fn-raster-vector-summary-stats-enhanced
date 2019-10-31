dbbb8f5b7caa89c9ffe8e98d3cead3f95ebe646c
manqoba <manqoba.sukati@peoplesized.com>
2019-10-31 09:41:44 +0200

    looks ready for template...

---
+++ fn-raster-vector-summary-stats-enhanced/handler.py
@@ -0,0 +1,42 @@
+import json
+import sys
+
+import fiona
+from rasterstats import zonal_stats, point_query
+from geojson import FeatureCollection
+
+
+def run_function(params):
+    # Prepare the parameters
+    subject_ref = params['subject']
+    raster      = params['raster']
+    stats       = params.get('stats', 'mean')
+    geojson_out = params.get('geojson_out', True)
+
+    try:
+        subject = fiona.open(subject_ref)
+        subject_types = get_subject_types(subject)
+
+        if 'Polygon' in subject_types or 'MultiPolygon' in subject_types:
+            features = zonal_stats(subject, raster, stats=stats, geojson_out=geojson_out)
+        elif 'Point' in subject_types or 'MultiPoint' in subject_types:
+            features = point_query(subject, raster, geojson_out=geojson_out)
+        else:
+            raise ValueError("Input features need to be either all Polygons or Points. Doesn't look like they are.")
+
+        # Decorate return as either GeoJSON FeatureCollection (other way to do this?) or just the array of stats
+        if geojson_out:
+            return FeatureCollection(features)
+        else:
+            return features
+    except ValueError as e:
+        if e == 'Specify either bounds or window':
+            raise ValueError("One or more features are lacking geometry or have null/empty values")
+
+    except AttributeError:
+        raise ValueError("Error calculating zonalstats. Please confirm every input "
+                         "geometry is valid, and contains coordinates")
+
+
+def get_subject_types(subject):
+    return set([feature['geometry']['type'] for feature in subject])
